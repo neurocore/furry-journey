2fefeddf1a9458284afa52296464f36ffc819a89
Nick Kurgin <nkurgin@yandex.ru>
2022-04-13 21:27:14 +0700

    Added migrations

---
+++ database/migrations/2022_04_12_100000_create_roles_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onUpdate('cascade')
+                  ->onDelete('no action');
+
+            $table->foreign('role_id')
+                  ->references('id')
+                  ->on('roles')
+                  ->onUpdate('cascade')
+                  ->onDelete('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_role');
+        Schema::dropIfExists('roles');
+    }
+}
---
+++ database/migrations/2022_04_12_300000_create_genres_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGenresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genres', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('book_genres', function (Blueprint $table) {
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('genre_id');
+            $table->timestamps();
+
+            $table->foreign('book_id')
+                  ->references('id')
+                  ->on('books')
+                  ->onUpdate('cascade')
+                  ->onDelete('no action');
+
+            $table->foreign('genre_id')
+                  ->references('id')
+                  ->on('genres')
+                  ->onUpdate('cascade')
+                  ->onDelete('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_genres');
+        Schema::dropIfExists('genres');
+    }
+}
