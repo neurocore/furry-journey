2141b929b197dc65ef4ef0b656a02817b4d7dd90
Nick Kurgin <nkurgin@yandex.ru>
2022-05-03 22:10:31 +0700

    Added api routes that doesn't require auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Book;
 use Illuminate\Http\Request;
 
 /*
@@ -13,6 +15,61 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// (-) 3.1. User authorization request
+
+
+
+// (+) 3.2. Getting books by author
+
+Route::get('/books', function(Request $request) {
+    return Book::with(['authors'])->get();
+});
+
+// (+) 3.3. Getting book by id
+
+Route::get('/books/{id}', function($id) {
+    return Book::findOrFail($id);
+});
+
+// (-) 3.4. Updating book by id ------------------- [auth required]
+
+Route::put('/books/{id}', function(Request $request, $id) {
+    $book = Book::findOrFail($id);
+    $book->update($request->all());
+
+    return $book;
+});
+
+// (-) 3.5. Deleting book by id ------------------- [auth required]
+
+Route::delete('/books/{id}', function($id) {
+    Book::findOrFail($id)->delete();
+
+    return 204;
+});
+
+// (+) 3.6. Getting authors list with books count
+
+Route::get('/authors', function() {
+    return User::whereHas('roles', function($q) { $q->where('name', 'author'); })
+             ->withCount(['books'])->get();
+});
+
+// (+) 3.7. Getting author info with books list
+
+Route::get('/authors/{id}', function($id) {
+    $user = User::findOrFail($id);
+
+    return $user->hasRole('author')
+         ? $user->with(['books'])->get()
+         : App::abort(404);
+});
+
+// (-) 3.8. Updating author info ------------------ [auth required]
+
+Route::put('/books/{id}', function(Request $request, $id) {
+    $user = User::findOrFail($id);
+    $user->update($request->all());
+
+    return $user;
 });
